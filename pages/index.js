9067a3fb82bbec633edab8487436dbf23613febd
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2022-10-08 11:24:45 +0700

    wip

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
                         <a target='_blank' rel='noreferrer' href='https://parsinta.com'>
                             Parsinta
                         </a>{' '}
-                        react course. .
+                        react course.
                     </p>
                     <code className={styles.code}>
                         <a href='https://paranoid-react.vercel.app/api/users' target='_blank' rel='noopener noreferrer'>
